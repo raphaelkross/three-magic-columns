6d48a352979c650838b8665916ef045e8f31d5f4
Rafael Angeline <me@rafaelangeline.com>
2020-01-11 22:31:56 -0200

    Remove the need to use window global at save

--- src/components/SaveState.js
+++ src/components/SaveState.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { usePreviousValue } from 'beautiful-react-hooks'; 
 import { useEditor } from "@craftjs/core";
 import lz from "lzutf8";
 
@@ -7,18 +8,16 @@ export const SaveState = () => {
         draggedNodeId: state.events.dragged,
     }));
 
+    const previousNodeId = usePreviousValue(draggedNodeId);
+
     const saveEditor = () => {
         const json = query.serialize();
         console.log('Saved', json);
         const state = lz.encodeBase64(lz.compress(json));
         localStorage.setItem('three-cols-editor', state);
     }
-    
-    if ( draggedNodeId != null ) {
-        window.editorPreviousNodeId = draggedNodeId;
-    }
 
-    if ( window.editorPreviousNodeId != null && draggedNodeId == null ) {
+    if ( previousNodeId != null && draggedNodeId == null ) {
         saveEditor();
     }
     
